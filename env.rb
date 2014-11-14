b997a5828b6bc0d53286b3ee738032a8b6e50b98
Richard McGain <richard.mcgain@shopify.com>
2014-11-14 03:01:45 +0000

    add ctags to a git template so when I clone something I get ctags

--- env.rb
+++ env.rb
@@ -1,9 +1,31 @@
+require 'pry-byebug'
+
 dep 'env' do
   requires 'matcher'
   requires 'vim'
   requires 'vim-config'
   requires 'dotfiles'
   requires 'ag.bin'
+  requires 'ctags'
+end
+
+dep 'ctags' do
+  requires 'ctags.bin'
+  met? {
+    %w(post-commit post-merge post-checkout post-rewrite)
+      .map { |f| File.join(Dir.pwd, '.git-template', 'hooks', f) }
+      .all? { |f| File.exists?(f) }
+  }
+  meet {
+    dir = File.join(Dir.home, '.git-template')
+    Dir.mkdir(dir) unless Dir.exists?(dir)
+    FileUtils.cp_r(File.join(File.expand_path(File.dirname(__FILE__)), 'git-template', 'hooks'), dir)
+  }
+end
+
+dep 'ctags.bin' do
+  installs 'exuberant-ctags'
+  provides 'ctags'
 end
 
 dep 'vim' do
